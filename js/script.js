49de6a66646f39a67f65577bf21628680e8fb2f3
Rainer Volz <dev@textmulch.de>
2012-04-04 17:14:07 +0200

    Added: change active link in navbar

--- js/script.js
+++ js/script.js
@@ -1,10 +1,18 @@
-/* Author:
-
+/* Author: Rainer Volz
 */
 
+jQuery(function(){
 
+	// Change the active link in the navbar
+	var changeActive = function(target) {
+		$("ul.nav li").removeClass("active")
+		target.addClass("active");
+	}
+	if (document.URL.match(/titles/)) {
+		changeActive($("#nav_titles"));
+	} else if (document.URL.match(/authors/)) {
+		changeActive($("#nav_authors"));
+	}
 
-
-
-
+});
 
