88c9f436d05a8196108101f7585d3a27141f9a11
Rainer Volz <dev@textmulch.de>
2012-11-22 22:29:18 +0100

    Added GD check to installcheck

--- installcheck.php
+++ installcheck.php
@@ -45,24 +45,44 @@ function fw($file) {
 	return (file_exists($file) && is_writeable($file));
 }
 
+function get_gd_version() { 
+  ob_start(); 
+  phpinfo(8); 
+  $module_info = ob_get_contents(); 
+  ob_end_clean(); 
+  if (preg_match("/\bgd\s+version\b[^\d\n\r]+?([\d\.]+)/i", $module_info,$matches)) { 
+      $gd_version_number = $matches[1]; 
+  } else { 
+      $gd_version_number = 0; 
+  } 
+  return $gd_version_number; 
+} 
+
+
 $srv = $_SERVER['SERVER_SOFTWARE'];
 $is_a = is_apache($srv) ;
 if ($is_a)
 	$mre =  mod_rewrite_enabled();
 else
 	$mre = false;
-
+$gdv = get_gd_version();
+if ($gdv >= 2)
+	$gde = true;
+else 
+	$gde = false;
 
 $template = $twig->loadTemplate('installcheck.html');
 echo $template->render(array(
 	'page' => array(
 		'rot' => '', #$app->request()->getRootUri(),
-		'version' => '0.9.3'
+		'version' => '0.9.4'
 	),
 	'is_a' => $is_a,
 	'srv' => $srv,
 	'mre' => $mre,
 	'hsql' => has_sqlite(),
+	'hgd2' => $gde,
+	'hgd2v' => $gdv,
 	'dwrit' => fw('./data'),
 	'mwrit' => fw('./data/data.db')
 	));
