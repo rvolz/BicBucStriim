c0a5d82ab163252afed32a45add90349e7eb9407
Rainer Volz <dev@textmulch.de>
2015-05-27 18:19:37 +0200

    Updated version info for 1.2.5

--- installcheck.php
+++ installcheck.php
@@ -125,7 +125,7 @@ $template = $twig->loadTemplate('installcheck.html');
 echo $template->render(array(
 	'page' => array(
 		'rot' => '', 
-		'version' => '1.2.5a'
+		'version' => '1.2.5'
 	),
 	'is_a' => $is_a,
 	'srv' => $srv,
