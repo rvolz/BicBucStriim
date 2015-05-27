fd7f234dec7a209dbb9ddf74038e9d91c5e0109f
Rainer Volz <dev@textmulch.de>
2015-05-27 18:22:29 +0200

    Updated versions for development 1.2.6a

--- Rakefile.rb
+++ Rakefile.rb
@@ -15,7 +15,7 @@ require 'yaml'
 require 'logger'
 
 APPNAME = 'BicBucStriim'
-VERSION = '1.2.5'
+VERSION = '1.2.6a'
 
 SOURCE = "."
 LESS = File.join( SOURCE, "style")
--- installcheck.php
+++ installcheck.php
@@ -125,7 +125,7 @@ $template = $twig->loadTemplate('installcheck.html');
 echo $template->render(array(
 	'page' => array(
 		'rot' => '', 
-		'version' => '1.2.5'
+		'version' => '1.2.6a'
 	),
 	'is_a' => $is_a,
 	'srv' => $srv,
