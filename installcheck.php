b6e93ec81f0b021e7aef43fe231d8dd1a298bb4d
Rainer Volz <dev@textmulch.de>
2015-10-01 17:49:52 +0200

    Start new release cycle

--- installcheck.php
+++ installcheck.php
@@ -125,7 +125,7 @@ $template = $twig->loadTemplate('installcheck.html');
 echo $template->render(array(
 	'page' => array(
 		'rot' => '',
-		'version' => '1.3.0'
+		'version' => '1.3.1a'
 	),
 	'is_a' => $is_a,
 	'srv' => $srv,
