a1735b0d546421736359c527cb4791a9d1744dec
Rainer Volz <dev@textmulch.de>
2015-10-03 18:07:48 +0200

    Prepare for release 1.3.2

--- index.php
+++ index.php
@@ -33,7 +33,7 @@ $fallbackLang = 'en';
 # Application Name
 $appname = 'BicBucStriim';
 # App version
-$appversion = '1.3.2a';
+$appversion = '1.3.2';
 
 # Init app and routes
 $app = new \Slim\Slim(array(
--- installcheck.php
+++ installcheck.php
@@ -125,7 +125,7 @@ $template = $twig->loadTemplate('installcheck.html');
 echo $template->render(array(
 	'page' => array(
 		'rot' => '',
-		'version' => '1.3.2a'
+		'version' => '1.3.2'
 	),
 	'is_a' => $is_a,
 	'srv' => $srv,
