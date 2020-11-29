3668bd763773de1a91c867d58f1d44b16dcd7c9d
Rainer Volz <dev@textmulch.de>
2020-11-29 11:31:23 +0100

    Unified list views

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,45 @@
 module.exports = {
-    purge: ["./public/**/*.html"],
+    purge: [],
     theme: {
-        extend: {},
+        extend: {
+            card: theme => ({
+                maxWidth: '20rem'
+            })
+        },
     },
     variants: {},
     plugins: [
         require('@tailwindcss/forms'),
+        // see https://github.com/NathanHeffley/tailwindcss-card
+        module.exports = function({ addComponents, e, theme }) {
+            const defaultTheme = {
+                maxWidth: theme('maxWidth.sm', '24rem'),
+                borderRadius: theme('borderRadius.default', '.25rem'),
+                boxShadow: theme('boxShadow.lg', '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)'),
+                padding: `${theme('padding.4', '1rem')} ${theme('padding.6', '1.5rem')}`,
+            }
+
+            const userTheme = theme('card', {})
+
+            const cardTheme = {...defaultTheme, ...userTheme}
+
+            addComponents({
+                '.card': {
+                    maxWidth: cardTheme.maxWidth,
+                    borderRadius: cardTheme.borderRadius,
+                    boxShadow: cardTheme.boxShadow,
+                    overflow: 'hidden',
+                },
+
+                '.card-image': {
+                    display: 'block',
+                    width: '100%',
+                },
+
+                '.card-content': {
+                    padding: cardTheme.padding,
+                },
+            })
+        },
     ],
 };
