d2c867a81b85ed80af1dba42a964e12fb94df206
Rainer Volz <dev@textmulch.de>
2021-01-09 19:07:20 +0100

    Adding a sticky header solved also positioning problems for navbar popups.

--- scripts/download-js.sh
+++ scripts/download-js.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 AMH=0.5.1
+ATDA=1.0.1
 wget -O alpine-magic-helpers-${AMH}.min.js https://cdn.jsdelivr.net/gh/alpine-collective/alpine-magic-helpers@${AMH}/dist/index.min.js
+wget -O alpine-turbo-drive-adapter-${ATDA}.min.js https://cdn.jsdelivr.net/npm/alpine-turbo-drive-adapter@${ATDA}/dist/alpine-turbo-drive-adapter.min.js
